2c047c5bd861d9ec9cc24ead4a16d32c06ab138f
Cody Burrows <code1010@rochester.rr.com>
2015-05-27 13:32:18 -0400

    About to make the gui components

--- src/Game.java
+++ src/Game.java
@@ -16,20 +16,23 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
-public class Window extends JPanel {
+public class Game extends JPanel {
 
 	public static void main(String [] args) throws InterruptedException{
-		JFrame frame = new JFrame("Wire Warfare");
-		Window window = new Window();
+		JFrame frame = new JFrame("Vengeance");
+		Game window = new Game();
 		frame.add(window);
-//		BufferedImage icon;
-//		try {
-//			icon = ImageIO.read(new File("src/WireWarfareLogo.png"));
-//			frame.setIconImage(icon);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		
+		//set the icon
+		BufferedImage icon;
+		try {
+			icon = ImageIO.read(new File("src/vengeance.jpg"));
+			frame.setIconImage(icon);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		frame.setSize(907, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -42,18 +45,8 @@ public class Window extends JPanel {
 		super.paint(g);
 		Graphics2D gd = (Graphics2D) g;
 		gd.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
-//		gd.setColor(Color.GRAY);
-//		gd.fillRect(0, 0, 907, 550);
-//		try {
-//			BufferedImage img;
-//			img = ImageIO.read(new File("src/border.png"));
-//			gd.drawImage(img, -20, -20, null);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
+
+		//paint stuff hear
 		
 	}
 	
